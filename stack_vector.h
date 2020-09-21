968b81de32364895803035312fa4a23aa4c5c85d
Denis <denis.poroshin98@gmail.com>
2020-09-21 15:48:23 +0300

    solution with array

--- stack_vector.h
+++ stack_vector.h
@@ -2,28 +2,64 @@
 #define EXPRESSES_STACK_VECTOR_H
 
 #include <stdexcept>
+#include <array>
 using namespace std;
 
 template <typename T, size_t N>
 class StackVector {
 public:
-    explicit StackVector(size_t a_size = 0);
+    explicit StackVector(size_t a_size = 0) : size(a_size) {
+        if (size > N) {
+            throw invalid_argument("too large size");
+        }
+    }
 
-    T& operator[](size_t index);
-    const T& operator[](size_t index) const;
+    T& operator[](size_t index) {
+        return data[index];
+    }
+    const T& operator[](size_t index) const {
+        return data[index];
+    }
 
-    ??? begin();
-    ??? end();
-    ??? begin() const;
-    ??? end() const;
+    auto begin() {
+        return data.begin();
+    }
+    auto end() {
+        return data.begin() + size;
+    }
+    auto begin() const {
+        return data.begin();
+    }
+    auto end() const {
+        return data.begin() + size;
+    }
 
-    size_t Size() const;
-    size_t Capacity() const;
+    size_t Size() const {
+        return size;
+    }
+    size_t Capacity() const {
+        return N;
+    }
 
-    void PushBack(const T& value);
-    T PopBack();
+    void PushBack(const T& value) {
+        if (size == N) {
+            throw overflow_error("...");
+        } else {
+            data[size++] = value;
+        }
+    }
+    T PopBack() {
+        if (size == 0) {
+            throw underflow_error("");
+        } else {
+            size--;
+            return data[size];
+        }
+    }
 
 private:
+    array<T, N> data;
+    size_t size = 0;
 };
 
 #endif //EXPRESSES_STACK_VECTOR_H
